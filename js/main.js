fb4611f6ad12c63d8ad26703748d74eeccb1cec8
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-04-07 22:58:46 -0400

    added toggle classes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ var $svg = $('svg');
 var $lines = $('.lines');
 var $engage = $('.engage');
 var $btn = $('.btn');
+
+$('svg').on('click', function (e) {
+  $('.lines').toggleClass('linify');
+  $('.engage').toggleClass('engaged');
+  $btn.toggleClass('button');
+  $('svg').toggleClass('is-pressed');
+});
